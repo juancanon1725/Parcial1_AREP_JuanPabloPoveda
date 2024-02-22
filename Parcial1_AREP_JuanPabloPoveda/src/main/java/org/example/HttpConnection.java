22b814c5fae14bcc1a35d680fca0cf968b2eb403
juancanon1725 <98672541+juancanon1725@users.noreply.github.com>
2024-02-22 16:09:39 -0500

    Add files via upload

---
+++ Parcial1_AREP_JuanPabloPoveda/src/main/java/org/example/HttpConnection.java
@@ -0,0 +1,50 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+public class HttpConnection {
+
+    private static final String USER_AGENT = "Mozilla/5.0";
+    private static final String GET_URL = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=fb&apikey=Q1QZFVJQ21K7C6XM";
+
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        URL obj = new URL(GET_URL);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+        //The following invocation perform the connection implicitly before getting the code
+        int responseCode = con.getResponseCode();
+        System.out.println("GET Response Code :: " + responseCode);
+
+        if (responseCode == HttpURLConnection.HTTP_OK) { // success
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            boolean firstLine = true ;
+            while ((inputLine = in.readLine()) != null) {
+                if (firstLine) {
+                    String[] newUri = inputLine.split("");
+                    URI uri = new URI("");
+                    response.append(inputLine);
+                    firstLine = false;
+                }
+            }
+            in.close();
+            // print result
+            System.out.println(response.toString());
+        } else {
+            System.out.println("GET request not worked");
+        }
+        System.out.println("GET DONE");
+    }
+
+    public void handlerHttpConnection(){
+    }
+} 
